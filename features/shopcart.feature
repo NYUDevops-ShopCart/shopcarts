87ef3b1e2a17f522f4086b6cc342f8b8c57e216d
Zihan Zhang <zzihan1203@gmail.com>
2019-11-16 15:59:05 -0500

    posting the changes so far

--- features/shopcart.feature
+++ features/shopcart.feature
@@ -5,39 +5,20 @@ Feature: Shopcart store service back-end
 
 Background:
     Given the following shopcart
-        | customer id   | product id    | item text     | quantity  |
-        | 1             | 1             | MacBook Pro   | 3         |
-        | 1             | 2             | iPhone        | 4         |
-        | 1             | 3             | Apple Watch   | 1         |
+        | customer id   | product id    | text          | quantity  | price     
+        | 1             | 1             | MacBook Pro   | 3         | 1000
+        | 1             | 2             | iPhone        | 4         | 800
+        | 2             | 3             | Apple Watch   | 1         | 400
+
 
 Scenario: The server is running
     When I visit the "Home Page"
     Then I should see "Shopcart RESTful Service" in the title
     And I should not see "404 Not Found"
 
-Scenario: Create a Item
-    When I visit the "Home Page"
-    And I set the "Customer ID" to "1"
-    And I set the "Product ID" to "4"
-    And I set the "Text" to "Coffee Machine"
-    And I set the "Quantity" to "2"
-    And I press the "Create" button
-    Then I should see the message "Success"
-    When I copy the "Id" field
-    And I press the "Clear" button
-    Then the "Customer ID" field should be empty
-    And the "Product ID" field should be empty
-    And the "Text" field should be empty
-    And the "Quantity" field should be empty
-    When I paste the "Id" field
-    And I press the "Retrieve" button
-    Then I should see "1" in the "Customer ID" field
-    And I should see "4" in the "Product ID" field
-    And I should see "Coffee Machine" in the "Text" field
-    And I should see "2" in the "Quantity" field
-
-#Scenario: List
-#Scenario: Query
+Scenario: Create 
+#Scenario: List 
+#Scenario: Query 
 #Scenario: Read
 #Scenario: Delete
 #Scenario: Update 
--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -3,24 +3,21 @@ $(function () {
     // ****************************************
     //  U T I L I T Y   F U N C T I O N S
     // ****************************************
-
-    // Updates the form with data from the response
     function update_form_data(res) {
-        $("#pet_id").val(res._id);
-        $("#pet_name").val(res.name);
-        $("#pet_category").val(res.category);
-        if (res.available == true) {
-            $("#pet_available").val("true");
-        } else {
-            $("#pet_available").val("false");
-        }
+        $("#customer_id").val(res.customer_ID);
+        $("#product_id").val(res.product_ID);
+        $("#item_text").val(res.item_text);
+        $("#quantity").val(res.quantity);
+        $("#price").val(res.price);
     }
 
     /// Clears all form fields
     function clear_form_data() {
-        $("#pet_name").val("");
-        $("#pet_category").val("");
-        $("#pet_available").val("");
+        $("#customer_id").val("");
+        $("#product_id").val("");
+        $("#item_text").val("");
+        $("#quantity").val("");
+        $("#price").val("")
     }
 
     // Updates the flash message area
@@ -32,7 +29,7 @@ $(function () {
     // ****************************************
     // Create a Pet
     // ****************************************
-
+    /*
     $("#create-btn").click(function () {
 
         var name = $("#pet_name").val();
@@ -125,11 +122,57 @@ $(function () {
         });
 
     });
+    */
+    // ****************************************
+    // List shopcart
+    // ****************************************
+
+    $("#list-btn").click(function () {
+
+        var customer_id = $("#customer_id").val();
+
+        var ajax = $.ajax({
+            type: "GET",
+            url: "/shopcarts/" + customer_id
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            $("#search_results").empty();
+            $("#search_results").append('<table class="table-striped" cellpadding="10">');
+            var header = '<tr>'
+            header += '<th style="width:10%">Customer_ID</th>'
+            header += '<th style="width:40%">Product_ID</th>'
+            header += '<th style="width:40%">Item_Text</th>'
+            header += '<th style="width:40%">Quantity</th>'
+            header += '<th style="width:10%">Price</th></tr>'
+            $("#search_results").append(header);
+            var firstItem = "";
+            for(var i = 0; i < res.length; i++) {
+                var item = res[i];
+                var row = "<tr><td>"+item.customer_id+"</td><td>"+item.product_id+"</td><td>"+item.item_text+"</td><td>"+item.quantity+"</td></tr>"+item.price+"</td><td>";
+                $("#search_results").append(row);
+                if (i == 0) {
+                    firstItem = item;
+                }
+            }
+
+            $("#search_results").append('</table>');
+            update_form_data(res)
+            flash_message("List shopcart Success!")
+        });
+
+        ajax.fail(function(res){
+            clear_form_data()
+            flash_message(res.responseJSON.message)
+        });
+
+    });
 
     // ****************************************
     // Delete a Pet
     // ****************************************
-
+    /*
     $("#delete-btn").click(function () {
 
         var pet_id = $("#pet_id").val();
@@ -232,5 +275,5 @@ $(function () {
         });
 
     });
-
+    */
 })
